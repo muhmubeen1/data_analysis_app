c67d0fc6297103c4f23764234be2dbb967fc11a5
muhmubeen1 <muhmubeen1@gmail.com>
2024-07-27 21:33:56 +0500

    create splash screen animation

--- data_flow/lib/Vieiw/splash_screen.dart
+++ data_flow/lib/Vieiw/splash_screen.dart
@@ -1,16 +1,81 @@
+import 'dart:async';
+import 'dart:math';
+
+// import 'dart:nativewrappers/_internal/vm/lib/async_patch.dart';
+import 'package:data_flow/Vieiw/world_states.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 5), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => WorldStates()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black12,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [],
+            children: [
+              AnimatedBuilder(
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    child: const Center(
+                      child: Image(
+                        image: AssetImage('images/virus.png'),
+                      ),
+                    ),
+                  ),
+                  animation: _controller,
+                  builder: (BuildContext context, Widget? child) {
+                    return Transform.rotate(
+                      angle: _controller.value * 2 * pi,
+                      child: child,
+                    );
+                  }),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.08,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Covid-19\nTracker App",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
           ),
         ),
       ),
