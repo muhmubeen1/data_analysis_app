a301be81c647fd7a9657e219b5c5b759fc6d9ae7
muhmubeen1 <muhmubeen1@gmail.com>
2024-07-30 21:28:03 +0500

    origin

--- data_flow/lib/Vieiw/countries_list.dart
+++ data_flow/lib/Vieiw/countries_list.dart
@@ -1,6 +1,7 @@
 import 'package:data_flow/Services/world_statsServices.dart';
 import 'package:data_flow/Vieiw/world_states.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CountriesList extends StatefulWidget {
   const CountriesList({Key? key}) : super(key: key);
@@ -33,6 +34,9 @@ class _CountriesListState extends State<CountriesList> {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
+                    onChanged: (value) {
+                      setState(() {});
+                    },
                     controller: textEditingController,
                     decoration: InputDecoration(
                         hintText: "Search by Country name",
@@ -47,20 +51,92 @@ class _CountriesListState extends State<CountriesList> {
                         builder:
                             (context, AsyncSnapshot<List<dynamic>> snapshot) {
                           if (!snapshot.hasData) {
-                            return Text("LOADING");
+                            return ListView.builder(
+                                itemCount: 8,
+                                itemBuilder: (context, index) {
+                                  return Shimmer.fromColors(
+                                    baseColor: Colors.grey.shade700,
+                                    highlightColor: Colors.grey.shade100,
+                                    child: Column(
+                                      children: [
+                                        ListTile(
+                                          title: Container(
+                                            height: 10,
+                                            width: 89,
+                                            color: Colors.white,
+                                          ),
+                                          subtitle: Container(
+                                            height: 10,
+                                            width: 89,
+                                            color: Colors.white,
+                                          ),
+                                          leading: Container(
+                                            height: 50,
+                                            width: 50,
+                                            color: Colors.white,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                });
                           } else {
                             return ListView.builder(
                                 itemCount: snapshot.data!.length,
                                 itemBuilder: (context, index) {
-                                  return Column(
-                                    children: [
-                                      ListTile(
-                                          leading: Image(
-                                              image: NetworkImage(snapshot
-                                                      .data![index]
-                                                  ["countryInfo"]["flag"]))),
-                                    ],
-                                  );
+                                  String name =
+                                      snapshot.data![index]["country"];
+                                  if (textEditingController.text.isEmpty) {
+                                    return Column(
+                                      children: [
+                                        ListTile(
+                                            title: Text(
+                                              snapshot.data![index]["country"],
+                                              style: TextStyle(
+                                                  color: Colors.white38),
+                                            ),
+                                            subtitle: Text(
+                                              snapshot.data![index]["cases"]
+                                                  .toString(),
+                                              style: TextStyle(
+                                                  color: Colors.white38),
+                                            ),
+                                            leading: Image(
+                                                height: 50,
+                                                width: 50,
+                                                image: NetworkImage(snapshot
+                                                        .data![index]
+                                                    ["countryInfo"]["flag"]))),
+                                      ],
+                                    );
+                                  } else if (name.toLowerCase().contains(
+                                      textEditingController.text
+                                          .toLowerCase())) {
+                                    return Column(
+                                      children: [
+                                        ListTile(
+                                            title: Text(
+                                              snapshot.data![index]["country"],
+                                              style: TextStyle(
+                                                  color: Colors.white38),
+                                            ),
+                                            subtitle: Text(
+                                              snapshot.data![index]["cases"]
+                                                  .toString(),
+                                              style: TextStyle(
+                                                  color: Colors.white38),
+                                            ),
+                                            leading: Image(
+                                                height: 50,
+                                                width: 50,
+                                                image: NetworkImage(snapshot
+                                                        .data![index]
+                                                    ["countryInfo"]["flag"]))),
+                                      ],
+                                    );
+                                  } else {
+                                    return Container();
+                                  }
                                 });
                           }
                         })),
